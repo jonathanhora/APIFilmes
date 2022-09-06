15cc0f7023f35521e5a25df822ab5223cf8e7147
Jonathan Hora <jonathan.hora@b2egroup.com.br>
2022-09-05 23:20:59 -0300

    Criando  Projeto
    Construção da API.
    Consulta e inserção de Filmes.
    Conexão com o MySQL.

---
+++ FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,46 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
+
+        [HttpPost]
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
+        {
+            filme.Id = id++;
+            filmes.Add(filme);
+            return CreatedAtAction(nameof(RecuperarFilmePorId), new { Id = filme.Id }, filme);
+        }
+
+        [HttpGet]
+        public IActionResult RecuperarFilmes()
+        {
+            return Ok(filmes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperarFilmePorId(int id)
+        {
+            Filme filme = filmes.FirstOrDefault(f => f.Id == id);
+
+            if(filme != null)
+            {
+                return Ok(filme);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
